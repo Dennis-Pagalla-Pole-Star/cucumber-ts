944c456218ccc84f2b32627876a7fc8dbf25bed6
Dennis-Pagalla-Pole-Star <dennis.pagalla@polestarglobal.com>
2021-06-11 17:20:54 +0530

    udpated feature file and step defs

--- src/client/users.ts
+++ src/client/users.ts
@@ -3,14 +3,13 @@ import randomstring from 'randomstring';
 var myarray: { id: any; username: any; password: any; salutation: any; firstName: any; lastName: any; jobTitle: any; emailAddress: string; homepageClassName: any; officePhoneNumber: any; }[] = [];
 
 export function create_users(no: number){
-    
     for(let i=1; i<=no;i++){
         myarray.push(create_single_user(i));
     }
-    return myarray;
-    
+    return myarray;  
 }
 
+/* randomstring npm module is used for data generation */
 export function create_single_user(id: number){
     var user = {
         id: id,
--- step-definitions/users.ts
+++ step-definitions/users.ts
@@ -1,4 +1,4 @@
-import { binding, given, then, when} from 'cucumber-tsflow';
+import { before, binding, given, then, when } from 'cucumber-tsflow';
 import { assert } from 'chai';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
@@ -11,38 +11,46 @@ export class UserManagement {
   data: any;
   status: any;
 
-  @given("a list of users")
+  /* Given statement will create mock response of 10 users 
+  with status code 200 */
+  @given(/a list of users/)
   public aListOfUsers() {
     this.mock.reset();
     this.mock.onGet("/api/v1/users").reply(200, create_users(10));
   }
 
-  @when("I send GET request for Get All Users")
+  /* Given statement will crete a mock response 
+  of 1 user with specified ID */
+  @given(/a single user with ID "([^"]*)"/)
+  public singleUser(id: number) {
+    this.mock.onGet("/api/v1/users/" + id).reply(200, create_single_user(id));
+  }
+
+  @when(/I send GET request for Get All Users/)
   public getAllUsers() {
-  axios.get("/api/v1/users").then( (response) =>{
-    this.data = response.data;
-    this.status = response.status;
-    assert(response);
-  });
+    axios.get("/api/v1/users").then((response) => {
+      this.data = response.data;
+      this.status = response.status;
+      console.log(this.data);
+      assert(this.data);
+    });
   }
 
   @then(/I verify status code "([^"]*)" is returned/)
   public verifyStatusCode(id: number) {
+    console.log(this.status);
     assert.equal(this.status, id);
   }
 
-  @given(/a single user with ID "([^"]*)"/)
-  public singleUser(id: number) {
-    this.mock.reset();
-    this.mock.onGet("/api/v1/users/"+id).reply(200,create_single_user(id));
-  }
-
-  @when(/I get user details for id "([^"]*)"/)
-  public getUserById(id: number){
-    axios.get("/api/v1/users/"+id).then( (response) => {
+  @when(/I send GET request for Get a single User with "([^"]*)"/)
+  public getUserById(id: number) {
+    axios.get("/api/v1/users/" + id).then((response) => {
       this.data = response.data;
       this.status = response.status;
-      assert(response);
-    })
+      console.log(this.data);
+      assert(this.data);
+    });
   }
+
+
 }
